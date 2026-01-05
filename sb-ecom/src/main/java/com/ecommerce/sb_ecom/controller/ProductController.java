54e561159960260ff4f8ef77a86acf8897e9d019
kaiff-ansari <mohdkaif8672@gmail.com>
2026-01-05 18:09:23 +0530

    added product module

---
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/controller/ProductController.java
@@ -0,0 +1,68 @@
+package com.ecommerce.sb_ecom.controller;
+
+import com.ecommerce.sb_ecom.model.Product;
+import com.ecommerce.sb_ecom.payload.ProductDTO;
+import com.ecommerce.sb_ecom.payload.ProductResponse;
+import com.ecommerce.sb_ecom.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/admin/categories/{categoryId}/product")
+    public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductDTO productDTO,
+                                                 @PathVariable Long categoryId){
+
+       ProductDTO savedProductDTO =  productService.addProduct(categoryId,productDTO);
+       return new ResponseEntity<>(savedProductDTO, HttpStatus.CREATED);
+
+
+    }
+
+    @GetMapping("/public/products")
+    public ResponseEntity<ProductResponse> getAllProducts(){
+
+        ProductResponse productResponse = productService.getAllProducts();
+        return new ResponseEntity<>(productResponse,HttpStatus.OK);
+    }
+
+    @GetMapping("/public/categories/{categoryId}/products")
+    public ResponseEntity<ProductResponse> getProductByCategory(@PathVariable Long categoryId){
+
+        ProductResponse productResponse = productService.searchByCategory(categoryId);
+        return new ResponseEntity<>(productResponse,HttpStatus.OK);
+    }
+
+    @GetMapping("/public/products/keyword/{keyword}")
+    public ResponseEntity<ProductResponse> getProductsByKeyword(@PathVariable String keyword){
+
+        ProductResponse productResponse = productService.searchProductByKeyword(keyword);
+        return new ResponseEntity<>(productResponse,HttpStatus.FOUND);
+
+    }
+
+    @PutMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> updateProduct(@RequestBody ProductDTO productDTO,
+                                                    @PathVariable Long productId){
+
+        ProductDTO updateProduct = productService.updateProduct(productId,productDTO);
+        return new ResponseEntity<>(updateProduct,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/admin/products/{productId}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable Long productId){
+
+       ProductDTO deleteProduct =  productService.deleteProduct(productId);
+       return new ResponseEntity<>(deleteProduct,HttpStatus.OK);
+
+
+    }
+
+}
---
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/service/ProductService.java
@@ -0,0 +1,19 @@
+package com.ecommerce.sb_ecom.service;
+
+import com.ecommerce.sb_ecom.payload.ProductDTO;
+import com.ecommerce.sb_ecom.payload.ProductResponse;
+
+public interface ProductService {
+    ProductDTO addProduct(Long categoryId, ProductDTO productDTO);
+
+    ProductResponse getAllProducts();
+
+    ProductResponse searchByCategory(Long categoryId);
+
+    ProductResponse searchProductByKeyword(String keyword);
+
+    ProductDTO updateProduct(Long productId, ProductDTO productDTO);
+
+    ProductDTO deleteProduct(Long productId);
+
+}
