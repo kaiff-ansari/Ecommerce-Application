cc098d7196035e19f3eb3664e428c4bd195f8c65
kaiff-ansari <mohdkaif8672@gmail.com>
2026-01-12 23:36:35 +0530

    fix endpoints

--- sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/jwt/JwtUtils.java
+++ sb-ecom/src/main/java/com/ecommerce/sb_ecom/security/jwt/JwtUtils.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.WebUtils;
 
